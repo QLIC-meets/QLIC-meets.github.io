a5689e4a6ced573be2144b76c2343d1e502c1ce8
Atul Singh Arora <toatularora@gmail.com>
2019-09-30 17:54:01 +0200

    updated

--- autoGit.sh
+++ autoGit.sh
@@ -1,3 +1,3 @@
 # git add . && git commit -am "Updated" && git push
 waitFor=${1:-500} 
-while true; do git add . && git commit -am "updated" && git push; sleep waitFor; done
+while true; do git add . && git commit -am "updated" && git push; sleep $waitFor; done
