a0bd40e2a39d72a51ae984f0dd5b8baf8b81aa03
Atul Singh Arora <toatularora@gmail.com>
2019-11-19 14:48:55 +0100

    updated at 19 Nov 2019 14:48:55

--- autoGit.sh
+++ autoGit.sh
@@ -2,5 +2,5 @@
 #falls back on 500 seconds if no parameter is passed to the script
 waitFor=${1:-500}
 #this fetches the date
-now=$(date)
-while true; do git add . && git commit -am "updated on $now" && git push; sleep $waitFor; done
+
+while true; do now=$(date); git add . && git commit -am "updated at $now" && git pull && git push; sleep $waitFor; done
